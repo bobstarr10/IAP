8cfe854988b14fdf18d7a74242e79d436594ed30
bobstarr10 <166503023+bobstarr10@users.noreply.github.com>
2024-09-09 09:56:36 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,7 @@
+<?php
+    require_once "load.php";
+    $ObjLayouts->heading();
+    $ObjMenus->main_menu();
+    $ObjContents->about_content();
+    $ObjContents->sidebar();
+    $ObjLayouts->footer();
---
+++ constants.php
@@ -0,0 +1,12 @@
+<?php
+
+define('DBTYPE', 'pdo');
+define('HOSTNAME', 'Localhost');
+define('DBPORT', '3306');
+define('DBNAME', 'ics_e');
+define('HOSTUSER', 'root');
+define('HOSTPASS', '');
+
+
+
+print HOSTNAME;
